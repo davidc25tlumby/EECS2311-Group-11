68ee5754f05bc9a0e5634ff427a29712c46969e1
Huy Hoang Minh Cu <kingofdragon1998@gmail.com>
2018-05-01 10:23:33 -0400

    reset

--- Enamel/src/authoringApp/AudioRecorder.java
+++ Enamel/src/authoringApp/AudioRecorder.java
@@ -1,33 +1,45 @@
 package authoringApp;
 
-import java.io.File;
-import java.io.IOException;
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.TargetDataLine;
-
+import javax.sound.sampled.*;
+import java.io.*;
+ 
+/**
+ * A sample program is to demonstrate how to record sound in Java
+ * author: www.codejava.net
+ */
 public class AudioRecorder {
-
-	File path = new File("C:\\Users\\Qasim Ahmed\\git\\EECS2311-Group-11\\Enamel\\FactoryScenarios\\AudioFiles\\Recordaudio.wav");
-	AudioFileFormat.Type type = AudioFileFormat.Type.WAVE;     //WAVE file type	
-	
-     
-	TargetDataLine data; 
-	
-	public static AudioFormat getformat() {
-		//audioformat
-		AudioFormat format = new AudioFormat(50000, 15, 2, true, true);    //2 is stereo
-		return format;
-	}
-	
-	//captures sound and records in type WAV format
-	public void startcapturingsound() {
-		try {
-            AudioFormat format = getformat();
+    // record duration, in milliseconds
+    static final long RECORD_TIME = 60000;  // 1 minute
+ 
+    // path of the wav file
+    File wavFile = new File("E:/Test/RecordAudio.wav");
+ 
+    // format of audio file
+    AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+ 
+    // the line from which audio data is captured
+    TargetDataLine line;
+ 
+    /**
+     * Defines an audio format
+     */
+    AudioFormat getAudioFormat() {
+        float sampleRate = 16000;
+        int sampleSizeInBits = 8;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
+                                             channels, signed, bigEndian);
+        return format;
+    }
+ 
+    /**
+     * Captures the sound and record into a WAV file
+     */
+    void start() {
+        try {
+            AudioFormat format = getAudioFormat();
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
  
             // checks if system supports the data line
@@ -35,60 +47,57 @@ public class AudioRecorder {
                 System.out.println("Line not supported");
                 System.exit(0);
             }
-            data = (TargetDataLine) AudioSystem.getLine(info);
-            data.open(format);
-            data.start();   // start capturing
-  
-            AudioInputStream inputstream = new AudioInputStream(data);
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();   // start capturing
+ 
+            System.out.println("Start capturing...");
+ 
+            AudioInputStream ais = new AudioInputStream(line);
  
             System.out.println("Start recording...");
  
-            // starts recording
-            
-            AudioSystem.write(inputstream, type, path);
+            // start recording
+            AudioSystem.write(ais, fileType, wavFile);
  
         } catch (LineUnavailableException ex) {
             ex.printStackTrace();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-    }  
-	public void finish() {               //closes data line to finish recording
-        data.stop();
-        data.close();
-        System.out.println("Done");
     }
-	
-	public static void main(String[] args) {
-		
-		final AudioRecorder audiorecorder = new AudioRecorder();
-		
-		Thread thread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				try {
-                    Thread.sleep(60000); //60 sec
+ 
+    /**
+     * Closes the target data line to finish capturing and recording
+     */
+    void finish() {
+        line.stop();
+        line.close();
+        System.out.println("Finished");
+    }
+ 
+    /**
+     * Entry to run the program
+     */
+    public static void main(String[] args) {
+        AudioRecorder recorder = new AudioRecorder();
+ 
+        // creates a new thread that waits for a specified
+        // of time before stopping
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(RECORD_TIME);
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
-                audiorecorder.finish();
+                recorder.finish();
             }
         });
  
-        thread.start();
+        stopper.start();
  
         // start recording
-        audiorecorder.startcapturingsound();
+        recorder.start();
     }
-	
-	{
-			
-		
-	}	
-	}
-	
-	
-
-	
+}
