ceda730cbb016eecc4b080bc23b7ce0f893d0c62
Huy Hoang Minh Cu <kingofdragon1998@gmail.com>
2018-05-01 22:25:26 -0400

    fix issue #6

--- Enamel/src/authoringApp/NewFileGUI.java
+++ Enamel/src/authoringApp/NewFileGUI.java
@@ -80,7 +80,7 @@ public class NewFileGUI extends javax.swing.JFrame {
         createButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         
         jLabel1.setText("Enter number of cell");
